a7c1e6e116bc96133e0ba8267f13988b5555b4b8
mati <mati@DESKTOP-90KPIC7>
2020-10-15 10:45:46 +0200

    Commit

---
+++ ComptaDies/src/comptadies/Main.java
@@ -0,0 +1,13 @@
+package comptadies;
+
+public class Main {
+
+	public static void main(String[] args) {		
+		
+		MyDate newDate = new MyDate();
+
+		int daysPerYear = newDate.returnDays(newDate.readDate());
+		System.out.println(newDate.returnWeekDay(daysPerYear));
+	}
+
+}
