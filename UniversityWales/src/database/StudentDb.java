e29ab5987d9e60c86dc33af6854f3f78a298a819
palicru01 <palicru01@gmail.com>
2020-10-20 19:16:15 +0200

    Commit

--- UniversityWales/src/database/StudentDb.java
+++ UniversityWales/src/database/StudentDb.java
@@ -1,10 +1,19 @@
 package database;
 
+import java.util.ArrayList;
+
+import model.Student;
+
 public class StudentDb {
 	
-	public static void studentDbPreFilling() {
-		
-		
+	public String name;
+	public static ArrayList<Student> students;
+	
+	public StudentDb(String name) {
+		super();
+		this.name = name;
 	}
+	
+
 
 }
