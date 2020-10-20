398c3da249edfb10d07bd7a324f8e2edc35960c4
mati <mati@DESKTOP-90KPIC7>
2020-10-20 13:34:22 +0200

    Commit

--- UniversityWales/src/model/IPersonAction.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public interface IPersonAction {
-	
-	public void getPersonalUniversityEmail();
-
-}
--- UniversityWales/src/model/Vaccine.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class Vaccine {
-	
-	public Date vaccineDay;
-	public String vaccineResult;
-	public String kindOfVaccine;
-
-}
