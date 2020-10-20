36520283fa62dc832f66ea70767173eb7bb1e9a9
mati <mati@DESKTOP-90KPIC7>
2020-10-20 13:58:35 +0200

    Commit

--- UniversityWales/src/model/Card.java
+++ UniversityWales/src/model/Card.java
@@ -3,6 +3,17 @@ package model;
 public class Card {
 	
 	protected int cardNumber;
+	private Date dateOfActivation;
+	protected String type;
 	protected double balance;
+
+	
+	public Card(Date dateOfActivation, String type, double balance) {
+		super();
+		this.cardNumber = hashCode();
+		this.dateOfActivation = dateOfActivation;
+		this.type = type;
+		this.balance = 0.00d;
+	}
 
 }
