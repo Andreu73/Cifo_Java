7fb25ecc646222cf288322724416025152230d29
mati <mati@DESKTOP-90KPIC7>
2020-10-22 14:01:34 +0200

    Commit

--- InterfacesHuman/src/model/Animal.java
+++ InterfacesHuman/src/model/Animal.java
@@ -14,18 +14,22 @@ public class Animal implements IAction{
 	
 	public void listen(){
 		
+		System.out.println("Listen to its Master!");
 	}
 	
 	public void eat(){
 		
+		System.out.println("It eats meat");
 	}
 	
 	public void play(){
 		
+		System.out.println("It is old");
 	}
 	
 	public void sleep(){
 		
+		System.out.println("It sleeps on the sofa");
 	}
 
 
--- InterfacesHuman/src/model/Cat.java
+++ InterfacesHuman/src/model/Cat.java
@@ -12,19 +12,24 @@ public class Cat extends Animal{
 	
 	public void listen(){
 		
+		System.out.println("Listen to its Master!");
 	}
 	
 	public void eat(){
 		
+		System.out.println("It eats a fish");
 	}
 	
 	public void play(){
 		
+		System.out.println("It plays with a dog");
 	}
 	
 	public void sleep(){
 		
+		System.out.println("It sleeps on the sofa");
 	}
+
 
 	
 }
