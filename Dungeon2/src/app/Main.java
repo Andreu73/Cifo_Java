08b8bdf8449b44168dae95a8940160839c0f615a
palicru01 <palicru01@gmail.com>
2020-10-22 21:09:52 +0200

    Commit

--- Dungeon2/src/app/Main.java
+++ Dungeon2/src/app/Main.java
@@ -1,9 +1,40 @@
 package app;
 
-import view.Dungeon;
+import model.XY;
 
 public class Main {
+
+	public static void main(String[] args) {
+		
+	final int LENGTH = 10;
+	final int HEIGHT = 20;
+	final int VAMPIRES = 3;
+	final int MOVES = 14;
 	
-	Dungeon dungeon = new Dungeon();
-	dungeon.
+//	XY xy = new XY();
+//	int x = xy.placeVampiresX();
+//	int y = xy.placeVampiresY();
+	
+	String[][] screen = new String[LENGTH][HEIGHT];
+		
+	for (int i = 0; i < LENGTH; i++) {
+
+		for (int j = 0; j < HEIGHT; j++) {
+			
+			screen[i][j]=".";
+		}
+	}
+
+	screen[4][5]="W";
+	
+	for (int i = 0; i < LENGTH; i++) {
+
+		for (int j = 0; j < HEIGHT; j++) {
+			
+			System.out.print(screen[i][j]);
+		}
+		System.out.println();
+	}
+	
+	}
 }
