a7789b20f3d9364467da7667b9411d17b026732c
palicru01 <palicru01@gmail.com>
2020-10-23 06:36:45 +0200

    Commit

--- Dungeon2/src/app/Main.java
+++ Dungeon2/src/app/Main.java
@@ -1,12 +1,14 @@
 package app;
 
 import java.util.Scanner;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import model.Hunter;
 import model.Screen;
 import service.VampireDb;
 
-public class Main {
+public class Main implements KeyListener{
 
 	public static void main(String[] args) {
 		
@@ -19,34 +21,54 @@ public class Main {
 	Hunter hunter = new Hunter("@");
 	
 	Screen.updateScreen(Screen.screen, hunter, VampireDb.vampires);
+//	keyPressed(e, hunter);
 
-
+	}
 	
-	while(true) {
+//	while(true) {
+//		
+//		String key = input.next();
+//		if(key.equals("a")) {
+//			hunter.moveUp();
+//			Screen.updateScreen(Screen.screen, hunter,VampireDb.vampires);
+//		}
+//		else if(key.equals("s")) {
+//			hunter.moveRight();
+//			Screen.updateScreen(Screen.screen, hunter, VampireDb.vampires);
+//		}
+//		else if(key.equals("d")) {
+//			hunter.moveDown();		
+//			Screen.updateScreen(Screen.screen, hunter,VampireDb.vampires);
+//		}
+//		else if(key.equals("w")) {
+//			hunter.moveLeft();
+//			Screen.updateScreen(Screen.screen, hunter,VampireDb.vampires);
+//		}
+//	}
+//
+//	
+//	}
+	
+//	public void keyPressed(KeyEvent e, Hunter hunter) {
+//		hunting(e, hunter);
+//    }
+	
+	public void hunting(KeyEvent e, Hunter hunter) {
 		
-		String key = input.next();
-		if(key.equals("a")) {
-			hunter.moveUp();
-			Screen.updateScreen(Screen.screen, hunter,VampireDb.vampires);
-		}
-		else if(key.equals("s")) {
-			hunter.moveRight();
-			Screen.updateScreen(Screen.screen, hunter, VampireDb.vampires);
-		}
-		else if(key.equals("d")) {
-			hunter.moveDown();		
-			Screen.updateScreen(Screen.screen, hunter,VampireDb.vampires);
-		}
-		else if(key.equals("w")) {
-			hunter.moveLeft();
-			Screen.updateScreen(Screen.screen, hunter,VampireDb.vampires);
-		}
-	}
+	    if (e.getKeyCode() == KeyEvent.VK_RIGHT ) {
+	    	hunter.moveRight();
+	    	Screen.updateScreen(Screen.screen, hunter, VampireDb.vampires);
+	    } else if (e.getKeyCode() == KeyEvent.VK_LEFT ) {
+	    	hunter.moveLeft();
+	    	Screen.updateScreen(Screen.screen, hunter, VampireDb.vampires);
+	    } else if (e.getKeyCode() == KeyEvent.VK_UP ) {
+	    	hunter.moveUp();
+	    	Screen.updateScreen(Screen.screen, hunter, VampireDb.vampires);
+	    } else if (e.getKeyCode() == KeyEvent.VK_DOWN ) {
+	    	hunter.moveDown();
+	    	Screen.updateScreen(Screen.screen, hunter, VampireDb.vampires);
+	    }
 
-	
 	}
-	
-
-	
 
 }
