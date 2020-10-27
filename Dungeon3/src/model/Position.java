20b058ce8368f0ad62d6cbc693687649f2a5c15b
palicru01 <palicru01@gmail.com>
2020-10-27 17:13:18 +0100

    Commit

--- Dungeon3/src/model/Position.java
+++ Dungeon3/src/model/Position.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import app.Main;
@@ -11,28 +12,51 @@ public class Position {
 	public int x;
 	public int y;
 //	public String[][] position = new String [Main.LENGTH][Main.HEIGHT];
-//	public String[][] position;
+	public int[] position = {x, y};
+	ArrayList<int[]> positionsArray = new ArrayList<>();
 	
 	public Position() {
 		super();
-
 //		this.x = x;
-//		this.y = y;		
-		initialNumberInLength();
-		initialNumberInHeight();
-//		this.position = new String[x][y];
+//		this.y = y;
+		this.position = checkPositionInArray();
+
 	}
 	Random random = new Random();
 
-	public void initialNumberInLength() {
+	public int[] checkPositionInArray() {
+		
+		int x = initialNumberInLength();
+		int y = initialNumberInHeight();
+		
+		int[]position2 = {x, y};
+		
+		for(int[] posisInt : positionsArray) {
+			
+		
+	    	for (int i = 0; i < position2.length; i++) {
+
+	            if (position2[i] != posisInt[i]) {
+	            	positionsArray.add(position2);
+	            	System.out.println("Ok");
+	            }
+	    	}
+		}
+    	return position2;
+		
+	}
+	
+	public int initialNumberInLength() {
 		int numberLength = random.nextInt(Main.LENGTH);
-		this.x=numberLength;
+//		this.x=numberLength;
+		return numberLength;
 
 	}
 	
-	public void initialNumberInHeight() {
+	public int initialNumberInHeight() {
 		int numberHeight = random.nextInt(Main.HEIGHT);
-		this.y=numberHeight;
+//		this.y=numberHeight;
+		return numberHeight;
 		
 	}
 	
---
+++ InterfacesHuman/src/app/Main.java
@@ -0,0 +1,11 @@
+package app;
+
+import model.Animal;
+
+public class Main {
+	
+	Animal animal = new Animal("AnimalName1",3);
+	animal.
+	
+
+}
